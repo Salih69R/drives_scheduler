490c70973c180d9eea7d0bbe2cb544e38426cb8a
SalihR <salihrmah@gmail.com>
2021-09-08 20:30:24 +0300

    base detailed car screen

---
+++ lib/UI/detailed_car.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:drives_scheduler/DATA/Model/car.dart';
+import 'package:drives_scheduler/DATA/Model/car_doc.dart';
+import 'package:drives_scheduler/DATA/Model/car_records.dart';
+import 'package:drives_scheduler/DATA/http_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DetailedCar extends StatefulWidget {
+  DetailedCar({Key? key, required this.http, required this.vehCode})
+      : super(key: key);
+  final HttpService http;
+  final String vehCode;
+
+  @override
+  State<StatefulWidget> createState() => _DetailedCarState(http, vehCode);
+}
+
+class _DetailedCarState extends State<DetailedCar> {
+  late CarRecords _carRecords;
+  final HttpService _http;
+  final String vehCode;
+  bool loading = true;
+
+  _DetailedCarState(this._http, this.vehCode);
+
+  @override
+  void initState() {
+    _getCarRecords();
+    super.initState();
+  }
+
+  Future _getCarRecords() async {
+    Response response;
+    try {
+      response = await _http.getCarRecords(vehCode);
+      if (response.statusCode == 200) {
+        setState(() {
+          loading = false;
+          _carRecords = CarRecords.fromJson(response.data);
+        });
+      } else {
+        print(
+            'something went wrong with the response(statusCode not 200): $response\n');
+      }
+    } on Exception catch (e) {
+      print('http get cars error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //load
+    return Center();
+  }
+}
