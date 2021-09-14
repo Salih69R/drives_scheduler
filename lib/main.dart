aff34400e378121699ea3fbfdaf6ba13ed29cd50
SalihR <salihrmah@gmail.com>
2021-09-14 23:00:57 +0300

    font a bit bigger

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Driver Scheduler',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.blue,
+          primaryTextTheme: TextTheme(
+            bodyText1: TextStyle(fontSize: 24), //tab title size
+            headline6: TextStyle(fontSize: 28), //app Bar title
+          ),
+          textTheme: TextTheme(
+            bodyText2:
+                TextStyle(fontSize: 20), //secondary list tile, and normal Text
+            subtitle1: TextStyle(fontSize: 22), //main list tile
+          )),
       home: Home(),
     );
   }
